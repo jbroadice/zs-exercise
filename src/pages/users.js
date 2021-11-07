4d7a4b6d9db1da7354361aa6bae8a2c54e399c6c
Jack Chapple <jack.chapple@kitchencut.com>
2021-11-07 09:08:05 +0000

    Sort users alphabetically.

--- src/pages/users.js
+++ src/pages/users.js
@@ -2,15 +2,23 @@ import { getUsers } from '@/data/users'
 
 export default function Users({ users }) {
   if (users === null) {
-    return <div>Could not retrieve users after multiple retries, and no cache yet exists. Please refresh to try again.</div>
+    return (
+      <div>
+        Could not retrieve users after multiple retries, and no cache yet exists.
+        Please refresh to try again.
+      </div>
+    )
   }
 
   return (
     <div>
       <h1>Users</h1>
-      {users.map(({ givenName, familyName }, i) => (
-        <div key={i}>{givenName} {familyName}</div>
-      ))}
+
+      {users
+        .sort((a, b) => a.givenName.localeCompare(b.givenName))
+        .map(({ givenName, familyName }, i) => (
+          <div key={i}>{givenName} {familyName}</div>
+        ))}
     </div>
   )
 }
